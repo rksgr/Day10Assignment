6cf2a8c9b5d8d6e7ca9abdfb2fe8c1daf1be9447
rksgr <92934865+rksgr@users.noreply.github.com>
2022-05-14 13:36:42 +0530

    Update AddressBookMain.java
    In the main method, the details are asked from the user.
    Rather create a separate method which will ask user for details.
    No need for setters in this file, as they have already been defined in other class.
    What is the purpose of getFirstName and getLastName methods?

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+// Desirable changes to be made in code, see the comments
 public class AddressBookMain {
 
     static List<AddressBookMain> addressList = new ArrayList();
